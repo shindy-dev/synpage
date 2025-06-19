07e509cd2a362db7e112522847745e95f25ffa08
shindy-dev <shindy.developer@gmail.com>
2025-06-20 00:51:38 +0900

    update

--- weblearningapp/urls.py
+++ weblearningapp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("pages/playground", views.playground, name="playground"),
     path("pages/count", views.count, name="count"),
     path("pages/table", views.table, name="table"),
+    path("api/data", view=views.api_data, name="api_data"),
 ]
--- weblearningapp/views.py
+++ weblearningapp/views.py
@@ -1,3 +1,7 @@
+import random
+import time
+
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -17,3 +21,19 @@ def count(request):
 
 def table(request):
     return render(request, "weblearningapp/pages/table.html")
+
+
+def api_data(request):
+    time.sleep(2)
+    response = {
+        "items": [
+            {
+                "ID": i,
+                "Name": f"Item {random.randint(1, 1000)}",
+                "Value": random.randint(1, 1000),
+            }
+            for i in range(1, 101)
+        ]
+    }
+
+    return JsonResponse(response)
