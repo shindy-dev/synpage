685b4afa87d500a4417eb8c7c4e44e6689648e84
shindy-dev <shindy.developer@gmail.com>
2025-06-20 00:31:14 +0900

    update

--- weblearningapp/urls.py
+++ weblearningapp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+
 from . import views
 
+app_name = "weblearningapp"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("pages/playground", views.playground, name="playground"),
+    path("pages/count", views.count, name="count"),
+    path("pages/table", views.table, name="table"),
 ]
--- weblearningapp/views.py
+++ weblearningapp/views.py
@@ -2,5 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
-    return render(request, 'weblearningapp/index.html')
+    return render(request, "weblearningapp/index.html")
+
+
+def playground(request):
+    return render(request, "weblearningapp/pages/playground.html")
+
+
+def count(request):
+    return render(request, "weblearningapp/pages/count.html")
+
+
+def table(request):
+    return render(request, "weblearningapp/pages/table.html")
