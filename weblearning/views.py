a294f17342dfa470a18827c8e16ebf63f807d72f
shindy-dev <shindy.developer@gmail.com>
2025-07-05 13:59:01 +0000

    documentsは動的に取得するよう変更

--- weblearning/views.py
+++ weblearning/views.py
@@ -7,7 +7,8 @@ from django.http import Http404, JsonResponse
 from django.shortcuts import render
 import markdown
 
-
+docs_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "markdown", "documents")
+docs_list = [ os.path.splitext(f)[0] for f in  os.listdir(docs_path) if os.path.isfile(os.path.join(docs_path, f))]
 
 def _markdown_page(request, dirpath, filename, title):
     # アプリのディレクトリを取得
@@ -22,27 +23,27 @@ def _markdown_page(request, dirpath, filename, title):
 
     md = markdown.Markdown(extensions=["toc", "attr_list", "md_in_html", 'fenced_code', 'tables'])
     html = md.convert(md_content)
-    return render(request, 'weblearning/markdown_page.html', {'content': html, 'title': title, 'toc_html': md.toc})
+    return render(request, 'weblearning/markdown_page.html', {'content': html, 'title': title, 'toc_html': md.toc, 'docs_list': docs_list})
 
 @login_required
 def index(request):
     return _markdown_page(request, 
                           dirpath="markdown", 
-                          filename="home.md", 
+                          filename="HOME.md", 
                           title="HOME")
     
 
 @login_required
 def playground(request):
-    return render(request, "weblearning/pages/playground.html",  {'title': "Playground"})
+    return render(request, "weblearning/pages/playground.html",  {'title': "Playground", 'docs_list': docs_list})
 
 @login_required
 def count(request):
-    return render(request, "weblearning/pages/count.html",  {'title': "Count"})
+    return render(request, "weblearning/pages/count.html",  {'title': "Count", 'docs_list': docs_list})
 
 @login_required
 def table(request):
-    return render(request, "weblearning/pages/table.html",  {'title': "Table"})
+    return render(request, "weblearning/pages/table.html",  {'title': "Table", 'docs_list': docs_list})
 
 @login_required
 def table_api_data(request):
@@ -62,10 +63,7 @@ def table_api_data(request):
 
 @login_required
 def docs(request, doc_type: str):
-    docs_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "markdown", "documents")
-    valid_docs = [ os.path.splitext(f)[0] for f in  os.listdir(docs_path) if os.path.isfile(os.path.join(docs_path, f))]
-    
-    if doc_type not in valid_docs:
+    if doc_type not in docs_list:
         raise Http404("Document type not found.")
     return _markdown_page(request, 
                           dirpath=os.path.join("markdown", "documents"), 
